12af347f386836461b275fe509808aaf112ea3ec
Erin Schoenhals <erin@kompressor.local>
2010-04-05 15:19:44 -0500

    Implemented Net::DAV::LockManager::UUID; replaced dummy lock token with one generated by generate() in Net::DAV::LockManager::_generate_token()

---
+++ lib/Net/DAV/LockManager/UUID.pm
@@ -0,0 +1,39 @@
+package Net::DAV::LockManager::UUID;
+
+use strict;
+
+use POSIX;
+use Digest::SHA qw(sha1_hex);
+
+sub generate {
+	my ($path, $author) = @_;
+
+	#
+	# Obtain a SHA1 sum concatenated from the following elements:
+	#
+	# * Filesystem path of item to receive a lock token
+	# * Lock owner/author
+	# * Current timestamp
+	# * Random number seed
+	# * Current UID
+	# * Process ID
+	#
+	my $sum = sha1_hex($path . $author . time() . rand() . getuid() . getpid());
+
+	#
+	# Split the SHA1 sum into a series of five tokens of varying
+	# lengths.  As per RFC 4918, every component of the UUID should
+	# be random, so as to not provide any information which may
+	# identify the host node generating said identifier.
+	#
+	my @tokens = ();
+	my $offset = 0;
+
+	foreach my $size (8, 4, 4, 4, 12) {
+		push @tokens, substr($sum, $offset += $size, $size);
+	}
+
+	return join "-", @tokens;
+}
+
+1;
