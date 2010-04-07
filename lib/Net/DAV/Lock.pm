6e4049d5402f469f35b59f1610bf7570ba5f3d82
Erin Schoenhals <erin@kompressor.local>
2010-04-07 11:00:06 -0500

    Fix issue where accessors are synthesized for the current package upon each invocation of the constructor; favor single quotes where no interpolation takes place

--- lib/Net/DAV/Lock.pm
+++ lib/Net/DAV/Lock.pm
@@ -2,31 +2,39 @@ package Net::DAV::Lock;
 
 use Net::DAV::UUID;
 
-our %properties = (
-    "expiry" => 0, "owner" => 0, "depth" => 0, "scope" => 0, "path" => 0, "uuid" => 1
+my %properties = (
+    'expiry' => 0, 'owner' => 0, 'depth' => 0, 'scope' => 0, 'path' => 0, 'uuid' => 1
 );
+
+sub INIT {
+    #
+    # Create read-only accessors for each property listed above.
+    #
+    foreach my $property (keys %properties) {
+        no strict 'refs';
+
+        *{$property} = sub {
+            my ($self) = @_;
+
+            return $self->{$property};
+        };
+    }
+}
 
 sub new {
     my ($class, $hash) = @_;
     my $obj = {};
 
+    #
+    # Copy the required parameters from the anonymous hash provided as
+    # input.  Die if any required values are missing.
+    #
     while (my ($property, $is_optional) = each(%properties)) {
-        #
-        # Create read-only accessors for each property listed above.
-        #
-        no strict "refs";
-
-        *{"$property"} = sub {
-            my ($self) = @_;
-
-            return $self->{$property};
-        };
-
         #
         # For non-optional arguments, ensure a value in the hash proviced
         # exists.
         #
-        unless ($hash->{$property} || $is_optional) {
+        unless (exists $hash->{$property} || $is_optional) {
             die("Missing value for '$property' property");
         }
 
@@ -40,8 +48,8 @@ sub new {
     # Calculate and store a new UUID based on the path and owner
     # specified, if none is present.
     #
-    unless ($hash->{"uuid"}) {
-        $obj->{"uuid"} = Net::DAV::UUID::generate($hash->{"path"}, $hash->{"owner"});
+    unless ($hash->{'uuid'}) {
+        $obj->{'uuid'} = Net::DAV::UUID::generate($hash->{'path'}, $hash->{'owner'});
     }
 
     return bless $obj, $class;
@@ -54,7 +62,7 @@ sub new {
 sub token {
     my ($self) = @_;
 
-    return "opaquelocktoken:" . $self->uuid;
+    return 'opaquelocktoken:' . $self->uuid;
 }
 
 1;
