696920d2b0a1c097408f5aa395fba1e11c7272a4
Erin Schoenhals <erin@kompressor.local>
2010-04-07 12:48:06 -0500

    Refactored Net::DAV::LockManager to use the new lock management database facilities; refactored, formatted, and commented existing pieces of code; added a new request validation static method which ensures a given request is valid for a particular lock, called _is_permitted()

--- lib/Net/DAV/LockManager.pm
+++ lib/Net/DAV/LockManager.pm
@@ -6,15 +6,16 @@ use warnings;
 use File::Spec ();
 use Net::DAV::UUID;
 
-my $MAX_LOCK_TIMEOUT = 15 * 60;
-my $DEFAULT_LOCK_TIMEOUT = $MAX_LOCK_TIMEOUT;
+my $MAX_LOCK_TIMEOUT        = 15 * 60;
+my $DEFAULT_LOCK_TIMEOUT    = $MAX_LOCK_TIMEOUT;
 
 sub new {
-    my $class = shift;
+    my ($class, $db) = (shift, shift);
     my %obj = @_;
-    $obj{'_locks'} = {};
 
-    return bless \%obj;
+    $obj{'db'} = $db;
+
+    return bless \%obj, $class;
 }
 
 sub can_modify {
@@ -22,22 +23,27 @@ sub can_modify {
 
     _validate_lock_request( $req );
 
-    my ($resource, $user, $token) = @{$req}{qw/path owner token/};
+    my ($resource, $token) = @{$req}{qw/path owner token/};
     my $lock = $self->_get_lock( $resource ) || $self->_get_indirect_lock( $resource );
 
     return 1 unless $lock;
     return unless $token;
 
-    return $lock->{'owner'} eq $user && $lock->{'token'} eq $token;
+    #
+    # Return based on whether or not any operations made by the requestor
+    # are permitted based on the current lock.
+    #
+    return _is_permitted( $req, $lock );
 }
 
 sub _get_indirect_lock {
     my ($self, $res) = @_;
 
-    while( $res =~ s{/[^/]+$}{} ) {
+    while ( $res =~ s{/[^/]+$}{} ) {
         $res = '/' unless length $res;
+
         my $lock = $self->_get_lock( $res );
-        return $lock if $lock && $lock->{'depth'} eq 'infinity';
+        return $lock if $lock && $lock->depth eq 'infinity';
     }
 
     return;
@@ -45,88 +51,196 @@ sub _get_indirect_lock {
 
 sub lock {
     my ($self, $req) = @_;
+
+    #
+    # Validate the given lock request.  Any errors in validation should
+    # percolate through the stack to this point.  It is the caller's
+    # responsibility to eval() for such conditions.
+    #
     _validate_lock_request( $req );
+
     my $path = $req->{'path'};
     my $timeout = $req->{'timeout'} || $DEFAULT_LOCK_TIMEOUT;
+
+    #
+    # If the lock timeout requested by the user is greater than the hard-coded
+    # maximum, then assume the value of the maximum.
+    #
     $timeout = $MAX_LOCK_TIMEOUT if $timeout > $MAX_LOCK_TIMEOUT;
-    my $expiry = time + $timeout;
+
+    #
+    # Calculate the expiration date of this lock based on the lifetime of the
+    # lock specified by the client in seconds.
+    #
+    my $expiry = time() + $timeout;
+
+    #
+    # Return with an undef unless this resource can be modified by the current
+    # user, and there is not already a lock present.
+    #
     return unless $self->can_modify( $req ) && !$self->_get_lock( $path );
 
-    my $token = $self->_generate_token( $req );
-
-    return $self->_set_lock( $path, {
-        expiry => $expiry,
-        owner => $req->{'owner'},
-        token => $token,
-        depth => (exists $req->{'depth'} ? $req->{'depth'} : 'infinity'),
-        scope => $req->{'scope'}||'exclusive',
-    });
+    #
+    # Create and return a new lock as added from the database with the information
+    # provided.  The new lock object, upon creation, will have calculated its own
+    # UUID/token information.
+    #
+    return $self->_add_lock(Net::DAV::Lock->new({
+        'path'      => $path,
+        'expiry'    => $expiry,
+        'owner'     => $req->{'owner'},
+        'depth'     => $req->{'depth'},
+        'scope'     => $req->{'scope'} || 'exclusive' # 0 or exclusive
+    }));
 }
 
 sub refresh_lock {
     my ($self, $req) = @_;
+
+    #
+    # Validate token specified in the request.
+    #
     _validate_lock_request( $req, 'token' );
 
+    #
+    # Attempt to obtain a lock which may be present for the current resource.
+    #
     my $lock = $self->_get_lock( $req->{'path'} );
 
-    return unless $lock && $lock->{'owner'} eq $req->{'owner'} && $lock->{'token'} eq $req->{'token'};
+    #
+    # Return early if there is not currently a lock on the resource, if the
+    # requestor is not the owner of the lock, or if the token specified by
+    # the client is not the same as the token presently assigned to the lock
+    # found in the database.
+    #
+    return unless _is_permitted( $req, $lock );
 
-    $lock->{'expiry'} = time() + ($req->{'timeout'}||$DEFAULT_LOCK_TIMEOUT);
+    #
+    # Renew the lock for the given interval as specified by the client.  If
+    # no timeout was specified by the client, then assume the default value
+    # as listed in this package.
+    #
+    my $expiry = time() + ($req->{'timeout'} || $DEFAULT_LOCK_TIMEOUT);
 
-    return $self->_set_lock( $req->{'path'}, $lock );
+    $lock->renew( $expiry );
+
+    #
+    # Finally, the lock may be updated.
+    #
+    return $self->_update_lock( $lock );
 }
 
 sub unlock {
     my ($self, $req) = @_;
+
+    #
+    # Ensure the token passed in the unlocking request is valid.
+    #
     _validate_lock_request( $req, 'token' );
 
+    #
+    # Attempt to obtain the lock based on the path specified in the request.
+    #
     my $lock = $self->_get_lock( $req->{'path'} );
 
-    return unless $lock && $lock->{'owner'} eq $req->{'owner'} && $lock->{'token'} eq $req->{'token'};
+    #
+    # Return early unless the request is appropriate for the current lock.
+    #
+    return unless _is_permitted( $req, $lock );
 
-    $self->_clear_lock( $req->{'path'} );
+    #
+    # Clear the lock without further ado.
+    #
+    $self->_clear_lock( $lock );
+
     return 1;
 }
 
 #
 # Retrieve a lock from the lock database, given the path to the lock.
-# Return undef if none.
+# Return undef if none.  This method also has the side effect of expiring
+# any old locks persisted upon fetching.
+#
 sub _get_lock {
     my ($self, $path) = @_;
-    my $lock = $self->{'_locks'}->{$path};
-    return $lock unless $lock;
 
-    if ( time >= $lock->{'expiry'} ) {
-        $self->_clear_lock( $path );
+    my $lock = $self->{'db'}->get( $path );
+
+    #
+    # Return undef if no lock was found.
+    #
+    return undef unless $lock;
+
+    #
+    # If the lock is past its expiration date, take the opportunity
+    # to invalidate it in the database.  Subsequently return undef.
+    #
+    if (time() >= $lock->expiry) {
+        $self->_clear_lock($lock);
+
         return undef;
     }
-    return $self->{'_locks'}->{$path};
+
+    return $lock;
 }
 
 #
-# Add a lock to the lock database, given a path and the lock information
-# TODO split into set and update
-sub _set_lock {
-    my ($self, $path, $lock) = @_;
-    $self->{'_locks'}->{$path} = $lock;
-    return { %$lock };
+# Add the given lock to the database.
+#
+sub _add_lock {
+    my ($self, $lock) = @_;
+
+    return $self->{'db'}->add($lock);
 }
 
 #
-# Remove a lock from the lock database
+# Update the lock provided.  Currently, only the expiration date
+# may be updated.
+#
+sub _update_lock {
+    my ($self, $lock) = @_;
+
+    return $self->{'db'}->update($lock);
+}
+
+#
+# Remove the lock object passed from the database.
+#
 sub _clear_lock {
-    my ($self, $path) = @_;
-    delete $self->{'_locks'}->{$path};
+    my ($self, $lock) = @_;
+
+    $self->{'db'}->remove($lock);
+
     return 1;
 }
 
 #
-# Generate a string appropriate for use as a LOCK token in a WebDAV
-# system, given the parameters in $req.
-sub _generate_token {
-    my ($self, $req) = @_;
+# Return true or false depending on whether or not the information reflected
+# in the request is appropriate for the lock obtained from the database.
+#
+sub _is_permitted {
+    my ($req, $lock) = @_;
 
-    return 'opaquelocktoken:' . Net::DAV::UUID::generate( $req->{'path'}, $req->{'owner'} );
+    #
+    # As no lock was passed, return false.
+    #
+    return 0 unless $lock;
+
+    #
+    # Return false if the current requestor is not the owner of the lock.
+    #
+    return 0 unless $req->{'owner'} eq $lock->owner;
+
+    #
+    # Return false if the token specified in the request is different from
+    # that of the lock passed.
+    #
+    return 0 unless $req->{'token'} eq $lock->token;
+
+    #
+    # The request passed is appropriate for this lock.
+    #
+    return 1;
 }
 
 #
