bc4d6902d9ed92a6947304b84725fc7a0f3aa0ec
G. Wade Johnson <wade@cpanel.net>
2010-06-30 15:22:28 -0500

    Update version number.

--- lib/Net/DAV/Server.pm
+++ lib/Net/DAV/Server.pm
@@ -16,7 +16,7 @@ use XML::LibXML::XPathContext;
 use Net::DAV::LockManager ();
 use Net::DAV::LockManager::DB ();
 
-our $VERSION = '1.300_05';
+our $VERSION = '1.300_06';
 $VERSION = eval $VERSION;  # convert development version into a simpler version number.
 
 our %implemented = (
